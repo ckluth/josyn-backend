745575d705c71f2c00af641fff097a56c1b89968
Kluth, Christofer <christofer.kluth@hzv.de>
2026-06-15 09:20:19 +0200

    wip: sync [2026-06-15 09:20]

--- db/db-config.ps1
+++ db/db-config.ps1
@@ -2,8 +2,11 @@
 # Single source of truth for local-dev database connection parameters.
 # Dot-source this file in all db scripts: . "$PSScriptRoot\db-config.ps1"
 
-$DbServer   = "localhost\SQLEXPRESS01"
-# $DbServer   = "(localdb)\MSSQLLocalDB"
+if ($env:COMPUTERNAME -eq "RZ-KNB784") {
+    $DbServer = "(localdb)\MSSQLLocalDB"
+} else {
+    $DbServer = "localhost\SQLEXPRESS01"
+}
 
 $DbDatabase = "josyn-db-local"
 $DbUser     = "tu.josyn"
